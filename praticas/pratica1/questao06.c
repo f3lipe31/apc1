c7c41e889894218ba1857edaccce6ec7918ebb1b
f3lipe31 <160795575+f3lipe31@users.noreply.github.com>
2024-03-28 00:54:53 +0000

    codigos da aula5 e pratica 1

--- praticas/pratica1/questao06.c
+++ praticas/pratica1/questao06.c
@@ -1,9 +1,36 @@
 // Faça um programa em C que calcule as raízes de uma equação do 2° grau através da fórmula de Bhaskara (-b +/- raiz_quadrada(b² - 4ac)/2a).
 
 #include <stdio.h>
+#include <math.h>
 
 
 int main(){
+  int a;
+  int b;
+  int c;
+
+  printf ("Insira o valor de a: ");
+  int deu_certo = scanf ("%i",&a);
+
+  printf ("Insira o valor de b: ");
+  deu_certo = scanf ("%i",&b);
+
+  printf ("Inseira o valor de c: ");
+  deu_certo = scanf ("%i",&c);
+
+  double delta = b * b - 4 * a* c;
+  double raiz_quadrada = sqrt (delta);
+  double raiz1 = (- b + raiz_quadrada) / (2 * a);
+  double raiz2 = (- b - raiz_quadrada) / (2 * a);
+
+  printf ("A raiz1 é: %.2f\n",raiz1);
+  printf ("A raiz2 é: %.2f\n",raiz2);
+
+  
+  
+  
+
+  
 
   
 
---
+++ praticas/pratica1/questao07.c
@@ -0,0 +1,33 @@
+//7. Faça um programa em C que calcule a altura alcançada por um avião após ter percorrido uma certa distância (seno(ângulo) = altura/distância). Considere o ângulo de inclinação do avião menor ou igual a 45°.
+
+  #include <stdio.h>
+  #include <math.h>
+
+
+int main (){
+  int angulo;
+  int distancia;
+  const double PI=3.14;
+  
+  printf("Insira o angulo: ");
+  int deu_certo = scanf ("%i",&angulo);
+
+  double seno = sin (angulo*(PI / 180));
+ 
+  printf("O seno do primeiro número é: %.2f\n",seno);
+
+  printf ("Entre com a distancia: ");
+  deu_certo = scanf ("%i",&distancia);
+  
+
+  double altura = seno* distancia;
+  printf ("A altura alcançada pelo avião foi: %.2f\n",altura);
+  
+  
+  
+ 
+  
+
+
+  return 0;
+}
