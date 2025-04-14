8327194b810327328c967f4e428afe6066d446e2
f3lipe31 <160795575+f3lipe31@users.noreply.github.com>
2025-04-14 12:39:01 +0000

    arividade

---
+++ estrtu_dados/heapsort.c
@@ -0,0 +1,28 @@
+int filho_esq(int i){
+  return 2*i+1;
+}
+int filho_dir(int i){
+  return 2*i+2;
+}
+
+void heap_max(int n,int arr[n],int i){
+  int i_maior = i;
+  int i_esq,i_dir,i_temp;
+  while (i_maior < n){ 
+    i_esq = filho_esq(i);
+    i_dir = filho_dir(i);
+    if (i_esq < n && arr[i_esq]>arr[i_maior]){
+      i_maior = i_esq;
+    }
+    if (i_dir < n && arr[i_dir]>arr[i_maior]){
+      i_maior = i_dir;
+    }
+    if (i_maior != i){
+      temp = arr[i];
+      arr[i]=arr[i_maior];
+      arr[i_maior] = temp;
+      i = i_maior;
+    }else{
+      break;
+    }
+}
